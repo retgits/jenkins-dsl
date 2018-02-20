88703784f97f2f8909cd84146a2e40bdd4a173a3
Leon Stigter <lstigter@gmail.com>
2018-02-20 13:31:57 -0800

    Create mirror_flogo_components.groovy

---
+++ projects/mirror_flogo_components.groovy
@@ -0,0 +1,52 @@
+// Project information
+String project = "flogo-components"
+String icon = "search.png"
+
+// GitHub information
+String gitHubRepository = "flogo-components"
+String gitHubUser = "retgits"
+
+// Gogs information
+String gogsRepository = "flogo-components"
+String gogsUser = "retgits"
+String gogsHost = "ubusrvls.na.tibco.com:3000"
+
+// Job DSL definition
+freeStyleJob("mirror-$project") {
+ displayName("mirror-$project")
+ description("Mirror github.com/$gitHubUser/$gitHubRepository")
+
+ checkoutRetryCount(3)
+
+ properties {
+  githubProjectUrl("https://github.com/$gitHubUser/$gitHubRepository")
+  sidebarLinks {
+   link("http://$gogsHost/$gogsUser/$gogsRepository", "Gogs", "$icon")
+  }
+ }
+
+ logRotator {
+  numToKeep(100)
+  daysToKeep(15)
+ }
+
+ triggers {
+  cron('@daily')
+ }
+
+ wrappers {
+  colorizeOutput()
+  credentialsBinding {
+   usernamePassword('GOGS_USERPASS', 'gogs')
+  }
+ }
+
+ steps {
+  shell("git clone --mirror https://github.com/$gitHubUser/$gitHubRepository repo")
+  shell("cd repo && git push --mirror http://\$GOGS_USERPASS@gogs:3000/$gogsUser/$gogsRepository")
+ }
+
+ publishers {
+  wsCleanup()
+ }
+}
