2f68a3877a86873ca8b25d3e385af8c65b3f9a44
retgits <lstigter@gmail.com>
2018-05-15 17:07:21 -0700

    Update Flogo Artifact Repository

--- projects/flogo_artifact_repository.groovy
+++ projects/flogo_artifact_repository.groovy
@@ -52,10 +52,14 @@ freeStyleJob("$project") {
   shell('#!/bin/bash\n' +
         'export GHACCESSTOKEN=$GHACCESSTOKEN\n' +
         'export TOMLFILE=./data/items.toml\n' +
+        'rm -rf public/*\n' +
         'go get -u github.com/nareix/curl\n' +
         'go get -u github.com/tomnomnom/linkheader\n' +
         'go run build.go\n' +
-        'hugo')
+        'wget -O hugo.tar.gz https://github.com/gohugoio/hugo/releases/download/v0.38.1/hugo_0.38.1_Linux-64bit.tar.gz\n' +
+        'mkdir -p hugobin\n' +
+        'tar -xzvf hugo.tar.gz -C ./hugobin\n' +
+        './hugobin/hugo')
  }
 
  publishers {
