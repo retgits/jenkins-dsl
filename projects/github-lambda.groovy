4c5f3d57a28303069a4f3fbc941d224ef353b34d
retgits <lstigter@gmail.com>
2018-05-22 13:47:56 -0700

    Add GitHub Lambda function

---
+++ projects/github-lambda.groovy
@@ -0,0 +1,71 @@
+// Project information
+String project = "GitHub - Lambda"
+String icon = "search.png"
+
+// Gogs information
+String gogsRepository = "github-lambda"
+String gogsUser = "retgits"
+String gogsHost = "ubusrvls.na.tibco.com:3000"
+String gogsInternalUrl = "gogs:3000"
+
+// Job DSL definition
+freeStyleJob("$project") {
+ displayName("$project")
+ description("Deploy $project to AWS Lambda")
+
+ checkoutRetryCount(3)
+
+ properties {
+  sidebarLinks {
+   link("http://$gogsHost/$gogsUser/$gogsRepository", "Gogs", "$icon")
+  }
+ }
+
+ scm {
+  git {
+   remote {
+    url("http://$gogsInternalUrl/$gogsUser/$gogsRepository")
+    credentials('gogs')
+   }
+   branches('*/master')
+   extensions {
+    wipeOutWorkspace()
+    cleanAfterCheckout()
+    relativeTargetDirectory("src/$gogsRepository")
+   }
+  }
+ }
+
+ logRotator {
+  numToKeep(100)
+  daysToKeep(15)
+ }
+
+ wrappers {
+  colorizeOutput()
+ }
+
+ environmentVariables(GOPATH: '$WORKSPACE')
+  
+ steps {
+  shell("#!/bin/bash\n" +
+        "#Set general properties for Lambda based deployments\n" +
+        "export AWS_ACCESS_KEY_ID=\$AWSACCESSKEYID\n" +
+        "export AWS_SECRET_ACCESS_KEY=\$AWSSECRETACCESSKEY\n" +
+        "export AWS_DEFAULT_REGION=\$AWSREGION\n" +
+        "find . -type f -name '*.sh' -exec chmod +x {} \\;\n" +
+        "#Perform the build\n" +
+        "cd src/$gogsRepository\n" +
+        "./build.sh deps\n" +
+        "./build.sh deploy\n")
+ }
+
+ publishers {
+  mailer {
+   recipients('$ADMIN_EMAIL')
+   notifyEveryUnstableBuild(true)
+   sendToIndividuals(false)
+  }
+  wsCleanup()
+ }
+}
