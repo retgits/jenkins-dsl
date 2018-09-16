7920c8196e53b7ab7f95627ca94d7e5607508a3c
retgits <lstigter@gmail.com>
2018-09-16 12:15:09 -0700

    Add new projects

---
+++ projects/mirror_contactform_lambda.groovy
@@ -0,0 +1,53 @@
+// Project
+String project = "contactform-lambda"
+String icon = "search.png"
+
+// Version Control
+String repository = "contactform-lambda"
+String user = "retgits"
+String gogs = "ubusrvls.na.tibco.com:3000"
+
+// Job DSL definition
+freeStyleJob("mirror $project") {
+ displayName("mirror $project")
+ description("Mirror github.com/$user/$repository")
+
+ checkoutRetryCount(3)
+
+ properties {
+  githubProjectUrl("https://github.com/$user/$repository")
+  sidebarLinks {
+   link("http://$gogs/$user/$repository", "Gogs", "$icon")
+  }
+ }
+
+ logRotator {
+  numToKeep(100)
+  daysToKeep(15)
+ }
+
+ triggers {
+  cron('@daily')
+ }
+
+ wrappers {
+  colorizeOutput()
+  credentialsBinding {
+   usernamePassword('GOGS_USERPASS', 'gogs')
+  }
+ }
+
+ steps {
+  shell("git clone --mirror https://github.com/$user/$repository repo")
+  shell("cd repo && git push --mirror http://\$GOGS_USERPASS@gogs:3000/$user/$repository")
+ }
+
+ publishers {
+  mailer {
+   recipients('$ADMIN_EMAIL')
+   notifyEveryUnstableBuild(true)
+   sendToIndividuals(false)
+  }
+  wsCleanup()
+ }
+}
