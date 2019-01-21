d1d0807bf57bdfcbf74204fbf68e372f3a266738
retgits <lstigter@gmail.com>
2019-01-21 10:04:41 -0800

    feat: add proxy build

---
+++ projects/build_nginx_proxy.groovy
@@ -0,0 +1,48 @@
+// Project
+String project = "nginx-proxy"
+String icon = "search.png"
+
+// Version Control
+String repository = "docker-configs"
+String user = "retgits"
+
+// Job DSL definition
+freeStyleJob("$project") {
+ displayName("$project")
+ description("Build $project")
+
+ checkoutRetryCount(3)
+
+ properties {
+  githubProjectUrl("https://github.com/$user/$repository")
+ }
+
+ logRotator {
+  numToKeep(100)
+  daysToKeep(15)
+ }
+
+ wrappers {
+  colorizeOutput()
+  credentialsBinding {
+   usernamePassword('GITHUB_USERPASS', 'github')
+  }
+ }
+
+ steps {
+  shell("git clone https://\$GITHUB_USERPASS@github.com/$user/$repository .")
+  shell("cd nginx-proxy")
+  shell("docker build . -t retgits/nginx-proxy:\$BUILD_NUMBER")
+  shell("docker stop nginx-proxy && docker rm nginx-proxy")
+  shell("docker run -d -p 80:80 --name=nginx-proxy retgits/nginx-proxy:\$BUILD_NUMBER")
+ }
+
+ publishers {
+  mailer {
+   recipients('$ADMIN_EMAIL')
+   notifyEveryUnstableBuild(true)
+   sendToIndividuals(false)
+  }
+  wsCleanup()
+ }
+}
