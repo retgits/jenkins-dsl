e196d633a9a0f6d0244d95f5078e28bedd96b6e9
retgits <lstigter@gmail.com>
2019-01-24 10:18:25 -0800

    fix: add config to webhook-bridge build

--- projects/build_webhook_bridge.groovy
+++ projects/build_webhook_bridge.groovy
@@ -37,7 +37,7 @@ freeStyleJob("$project") {
   shell("git clone https://github.com/$user/$repository .")
   shell("make deps")
   shell("make compile")
-  shell("git clone https://github.com/$user/docker-configs configs")
+  shell("git clone https://\$GITHUB_USERPASS@github.com/$user/docker-configs configs")
   shell("cp configs/webhook-bridge/config.yml ./out/config.yml")
   shell("cp configs/webhook-bridge/Dockerfile ./out/Dockerfile")
   shell("cd ./out && docker build . -t $user/$repository:\$BUILD_NUMBER")
