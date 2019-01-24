af53c301f5f4ea8311c0099db8346cc8f6f09e1b
retgits <lstigter@gmail.com>
2019-01-24 10:12:19 -0800

    fix: update build for webhook-bridge

--- projects/build_webhook_bridge.groovy
+++ projects/build_webhook_bridge.groovy
@@ -28,13 +28,19 @@ freeStyleJob("$project") {
 
  wrappers {
   colorizeOutput()
+  credentialsBinding {
+   usernamePassword('GITHUB_USERPASS', 'github')
+  }
  }
 
  steps {
   shell("git clone https://github.com/$user/$repository .")
   shell("make deps")
-  shell("make docker")
-  shell("docker tag $user/$repository:latest $user/$repository:\$BUILD_NUMBER")
+  shell("make compile")
+  shell("git clone https://github.com/$user/docker-configs configs")
+  shell("cp configs/webhook-bridge/config.yml ./out/config.yml")
+  shell("cp configs/webhook-bridge/Dockerfile ./out/Dockerfile")
+  shell("cd ./out && docker build . -t $user/$repository:\$BUILD_NUMBER")
  }
 
  publishers {
